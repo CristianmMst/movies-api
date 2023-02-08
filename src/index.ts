c7695a03a6ff3240a4a1cbd2057f1a36efcbe15a
CristianmMst <cristianmmst@gmail.com>
2023-02-07 20:53:14 -0500

    listening port

--- src/index.ts
+++ src/index.ts
@@ -5,9 +5,10 @@ import express from "express";
 import routes from "./routes";
 
 const server = express();
+const PORT = process.env.PORT || 3000;
 
 server.use(cors());
 server.use(express.json());
 server.use(routes);
 
-server.listen(3000, () => console.log("Server listening on port 3000"));
+server.listen(PORT, () => console.log("Server listening on port 3000"));
