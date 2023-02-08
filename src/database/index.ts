a35a93f850e1d64bdd370eabcb1ba231262405ac
CristianmMst <cristianmmst@gmail.com>
2023-02-07 20:45:20 -0500

    mongodb connect

--- src/database/index.ts
+++ src/database/index.ts
@@ -3,6 +3,6 @@ import mongoose from "mongoose";
 mongoose.set("strictQuery", false);
 
 mongoose
-  .connect("mongodb://0.0.0.0/movies")
+  .connect("mongodb://mongo:pSO4lXMrxVVVbdEEk1sv@containers-us-west-139.railway.app:5824")
   .then((db) => console.log(`connecting to database: ${db.connection.name}`))
   .catch((err) => console.error(err));
