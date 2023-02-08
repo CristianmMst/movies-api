7aa7ee91f5935e8e0a59ff3665a9e8bb33aa0c00
CristianmMst <cristianmmst@gmail.com>
2023-02-07 20:17:39 -0500

    database listening

--- src/database/index.ts
+++ src/database/index.ts
@@ -3,6 +3,6 @@ import mongoose from "mongoose";
 mongoose.set("strictQuery", false);
 
 mongoose
-  .connect("mongodb://127.0.0.1/movies")
+  .connect("mongodb://0.0.0.0/movies")
   .then((db) => console.log(`connecting to database: ${db.connection.name}`))
   .catch((err) => console.error(err));
